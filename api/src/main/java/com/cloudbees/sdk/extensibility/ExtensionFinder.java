8b908dfadd81f9624c7942802f235313cb212d9b
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-01-06 19:36:11 -0800

    MultiBinder doesn't work well with hierarchical extension points.
    This is because it wants to inject Set<X> for every extension point X,
    and so it ends up trying to inject Set<X> both in the parent and the
    child, which is an error.
    
    So we should just instead concentrate on defining bindings that doesn't
    collide, then define a mechanism to externally walk Injector and
    list up extension points.
    
    It is a shame that Guice doesn't allow applications and other modules
    to do injection-point aware injcetions --- this capability is reserved
    for a few built-in bindings, such as Logger.
    
    Originally-Committed-As: ae8c3d7176633d342453b1ba43b83cabeb0d6a58

--- api/src/main/java/com/cloudbees/sdk/extensibility/ExtensionFinder.java
+++ api/src/main/java/com/cloudbees/sdk/extensibility/ExtensionFinder.java
@@ -3,15 +3,13 @@ package com.cloudbees.sdk.extensibility;
 import com.google.inject.AbstractModule;
 import com.google.inject.BindingAnnotation;
 import com.google.inject.Key;
-import com.google.inject.multibindings.Multibinder;
 import org.jvnet.hudson.annotation_indexer.Index;
 import org.jvnet.hudson.annotation_indexer.Indexed;
 
+import javax.inject.Named;
 import java.io.IOException;
 import java.lang.annotation.Annotation;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
 
@@ -20,37 +18,25 @@ import java.util.logging.Logger;
  */
 public class ExtensionFinder extends AbstractModule {
     private final ClassLoader cl;
-    private final Set<Class> extensionPoints = new HashSet<Class>();
 
-    public ExtensionFinder(ClassLoader cl) throws IOException {
+    public ExtensionFinder(ClassLoader cl) {
         this.cl = cl;
-
-        for (Class c : Index.list(ExtensionPoint.class, cl, Class.class))
-            extensionPoints.add(c);
-    }
-    
-    public void addExtensionPoint(Class c) {
-        extensionPoints.add(c);
     }
 
     @Override
     protected void configure() {
         try {
-            // find all the extension points and build up multibinders
-            Map<Class,Multibinder> binders = new HashMap<Class, Multibinder>();
-            for (Class c : extensionPoints) {
-                Multibinder<?> mbind = Multibinder.newSetBinder(binder(), c);
-                binders.put(c,mbind);
-            }
-
             // find all extensions
             Set<Class> seen = new HashSet<Class>();
             for (Class<?> a : Index.list(ExtensionImplementation.class, cl, Class.class)) {
                 if (!a.isAnnotationPresent(Indexed.class))
                     throw new AssertionError(a+" has @ExtensionImplementation but not @Indexed");
                 for (Class c : Index.list(a.asSubclass(Annotation.class), cl, Class.class)) {
-                    if (seen.add(c))    // ... so that we don't bind the same class twice
-                        bind(c,c,binders.get(c));
+                    if (seen.add(c)) {// ... so that we don't bind the same class twice
+                        for (Class ext : listExtensionPoint(c,new HashSet<Class>())) {
+                            bind(c,ext);
+                        }
+                    }
                 }
             }
         } catch (IOException e) {
@@ -61,17 +47,18 @@ public class ExtensionFinder extends AbstractModule {
     /**
      * Allows the subtype to be selective about what to bind.
      */
-    protected <T> void bind(Class<? extends T> impl, Class<T> extensionPoint, Multibinder<T> mbinder) {
+    protected <T> void bind(Class<? extends T> impl, Class<T> extensionPoint) {
         Annotation bindingAnnotation = findBindingAnnotation(impl);
-        mbinder.addBinding().to(
-                bindingAnnotation!=null
-                ? Key.get(impl,bindingAnnotation)
-                : Key.get(impl));
+        if (bindingAnnotation==null)
+           // this is just to make it unique among others that implement the same contract
+            bindingAnnotation = AnnotationLiteral.of(Named.class,impl.getName());
+        bind(Key.get(extensionPoint,bindingAnnotation)).to(impl);
+        bind(impl);
     }
 
     private <T> Annotation findBindingAnnotation(Class<? extends T> impl) {
         for (Annotation a : impl.getAnnotations())
-            if (a.getClass().isAnnotationPresent(BindingAnnotation.class))
+            if (a.annotationType().isAnnotationPresent(BindingAnnotation.class))
                 return a;
         return null;
     }
